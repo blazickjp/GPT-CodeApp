2570f4e483162fb0be7e9c03309f43ad11167614
blazickjp <joe.blazick@yahoo.com>
2023-08-10 09:38:46 -0700

    AutoComplete for on demand functions

--- frontend/components/ChatInput.js
+++ frontend/components/ChatInput.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { AiOutlineSend } from 'react-icons/ai';
+import { Hint } from 'react-autocomplete-hint';
+
 
 const ChatInput = ({ onSubmit }) => {
     const [input, setInput] = useState('');
+    const commands = ['/CommandPlan', '/FileChange'];
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -25,14 +28,16 @@ const ChatInput = ({ onSubmit }) => {
 
     return (
         <div className="flex flex-row bg-gray-800 text-center justify-center items-center w-full text-black pb-5">
-            <form onSubmit={handleSubmit} className="flex w-1/2 relative">
-                <input
-                    type="text"
-                    value={input}
-                    onChange={(e) => setInput(e.target.value)}
-                    placeholder="Type a message..."
-                    className="p-2 rounded mr-2 flex-grow pr-10"
-                />
+            <form onSubmit={handleSubmit} className="relative w-1/2">
+                <Hint options={commands} allowTabFill>
+                    <input
+                        type="text"
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                        placeholder="Type a message..."
+                        className="p-2 rounded mr-2 flex-grow-2 pr-10 w-full" // Increase flex-grow and add w-full
+                    />
+                </Hint>
                 <button
                     type="submit"
                     className="absolute right-0 top-0 bottom-0 m-auto text-purple-700 font-bold py-2 px-4 rounded text-xl"
