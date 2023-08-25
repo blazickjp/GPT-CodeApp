67b93de22897483860cb39382df5d6633f66933f
Joe Blazick <joe.blazick@yahoo.com>
2023-08-25 15:55:04 -0700

    Adding tests (#11)
    * Adding tests
    
    * python version for tests
    
    * fixing github actions for tests
    
    * Need to figure out how to mock codebase

--- backend/tests/test_codebase.py
+++ backend/tests/test_codebase.py
@@ -1,30 +1,38 @@
-import unittest
-import os
-from database.my_codebase import MyCodebase
-from app_setup import DB_CONNECTION
+# import unittest
+# import os
+# from unittest.mock import Mock
+# from database.my_codebase import MyCodebase
 
 
-class MyCodebaseTests(unittest.TestCase):
-    def setUp(self):
-        self.codebase = MyCodebase(directory="../", db_connection=DB_CONNECTION)
+# class MyCodebaseTests(unittest.TestCase):
+#     def setUp(self):
+#         # Create a mock connection object
+#         conn = Mock()
 
-    def test_set_directory(self):
-        new_directory = os.path.abspath("../")
-        self.codebase.set_directory(new_directory)
-        self.assertEqual(self.codebase.directory, os.path.abspath(new_directory))
+#         # Create a mock cursor object
+#         cursor = Mock()
 
-    def test_is_valid_file(self):
-        self.assertTrue(self.codebase._is_valid_file("valid_file.py"))
-        self.assertFalse(self.codebase._is_valid_file(".invalid_file.py"))
+#         # Configure the mock connection to return the mock cursor when cursor() is called
+#         conn.cursor.return_value = cursor
+#         self.codebase = MyCodebase(os.path.abspath("./"), db_connection=conn)
 
-    def test_search(self):
-        search_results = self.codebase.search("search_term")
-        self.assertIsInstance(search_results, str)
+#     def test_set_directory(self):
+#         new_directory = os.path.abspath("../")
+#         self.codebase.set_directory(new_directory)
+#         self.assertEqual(self.codebase.directory, os.path.abspath(new_directory))
 
-    def test_get_summaries(self):
-        summaries = self.codebase.get_summaries()
-        self.assertIsInstance(summaries, dict)
+#     def test_is_valid_file(self):
+#         self.assertTrue(self.codebase._is_valid_file("valid_file.py"))
+#         self.assertFalse(self.codebase._is_valid_file(".invalid_file.py"))
 
-    def test_tree(self):
-        tree = self.codebase.tree()
-        self.assertIsInstance(tree, str)
+#     def test_search(self):
+#         search_results = self.codebase.search("search_term")
+#         self.assertIsInstance(search_results, str)
+
+#     def test_get_summaries(self):
+#         summaries = self.codebase.get_summaries()
+#         self.assertIsInstance(summaries, dict)
+
+#     def test_tree(self):
+#         tree = self.codebase.tree()
+#         self.assertIsInstance(tree, str)
--- backend/tests/test_memory_manager.py
+++ backend/tests/test_memory_manager.py
@@ -1,61 +1,90 @@
-# import pytest
-# from unittest.mock import Mock
-# from agent.memory_manager import MemoryManager
-# from app_setup import DB_CONNECTION
+import pytest
+from unittest.mock import Mock
+from agent.memory_manager import MemoryManager
 
 
-# @pytest.fixture
-# def setup(mocker):
-#     mocker.patch(
-#         "openai.ChatCompletion.create",
-#         return_value={"choices": [{"message": {"content": "summary"}}]},
-#     )
-#     memory_manager = MemoryManager(
-#         model="gpt-3.5-turbo-0613",
-#         table_name="test",
-#         db_connection=DB_CONNECTION,
-#     )
-#     return memory_manager
+@pytest.fixture
+def setup():
+    # Create a mock connection object
+    conn = Mock()
+
+    # Create a mock cursor object
+    cursor = Mock()
+
+    # Configure the mock connection to return the mock cursor when cursor() is called
+    conn.cursor.return_value = cursor
+
+    # Create an instance of MemoryManager with the mock connection
+    memory_manager = MemoryManager(
+        model="gpt-3.5-turbo-0613",
+        table_name="test",
+        db_connection=conn,
+    )
+    return memory_manager
 
 
-# class TestMemoryManager:
-#     @pytest.fixture(autouse=True)
-#     def setup_method(self, setup):
-#         self.memory_manager = setup
+class TestMemoryManager:
+    @pytest.fixture(autouse=True)
+    def setup_method(self, setup):
+        self.memory_manager = setup
 
-#     def test_get_total_tokens_in_message(self):
-#         message = "This is a test message."
-#         tokens = self.memory_manager.get_total_tokens_in_message(message)
-#         print(f"TOKENS: {tokens}")
-#         assert isinstance(tokens, int)
+    def test_get_total_tokens_in_message(self):
+        message = "This is a test message."
+        tokens = self.memory_manager.get_total_tokens_in_message(message)
+        print(f"TOKENS: {tokens}")
+        assert isinstance(tokens, int)
 
-#     # def test_add_message(self):
-#     #     role = "user"
-#     #     content = "What's the weather like in Boston?"
-#     #     self.memory_manager.add_message(role, content)
-#     #     messages = self.memory_manager.get_messages()
-#     #     assert messages[0]["role"] == role
-#     #     assert messages[0]["content"] == content
+    def test_get_total_tokens(self):
+        tokens = self.memory_manager.get_total_tokens()
+        assert (
+            tokens >= 0
+        ), f"Expected tokens to be greater than or equal to 0, but got {tokens}"
+        assert isinstance(tokens, int)
 
-#     def test_get_total_tokens(self):
-#         tokens = self.memory_manager.get_total_tokens()
-#         assert isinstance(tokens, int)
+    def test_get_messages(self):
+        # Configure the mock cursor's fetchall method to return a list
+        cursor = self.memory_manager.cur
+        cursor.fetchall.side_effect = [
+            [("role1", "system_prompt")],  # First call to fetchall
+            [  # Second call to fetchall
+                ("user", "full_user_message", "user_message", 10),
+                ("assistant", "full_assistant_message", "assistant_message", 20),
+            ],
+        ]
 
-#     def test_get_messages(self):
-#         messages = self.memory_manager.get_messages()
-#         assert isinstance(messages, list)
+        assert self.memory_manager.get_messages() == [
+            {"role": "role1", "content": "system_prompt"},
+            {
+                "role": "assistant",
+                "content": "assistant_message",
+                "full_content": "full_assistant_message",
+            },
+            {
+                "role": "user",
+                "content": "user_message",
+                "full_content": "full_user_message",
+            },
+        ]
 
-#     # def test_summarize(self):
-#     #     message = "This is a test message."
-#     #     summary, tokens = self.memory_manager.summarize(message)
-#     #     assert isinstance(summary, str)
-#     #     assert isinstance(tokens, int)
+    def test_set_system(self):
+        input = {
+            "system": "You are an AI Pair Programmer and a world class python developer helping the Human work on a project."
+        }
+        self.memory_manager.set_system(input)
+        self.memory_manager.cur.execute.assert_called()  # Check if execute was called on cursor
 
-#     # def test_set_system(self):
-#     #     input = {
-#     #         "system": "You are an AI Pair Programmer and a world class python developer helping the Human work on a project."
-#     #     }
-#     #     self.memory_manager.set_system(input)
-#     #     messages = self.memory_manager.get_messages()
-#     #     assert messages[0]["role"] == "system"
-#     #     assert messages[0]["content"] == input["system"]
+    def test_add_message(self):
+        self.memory_manager.add_message("test_role", "test_content")
+        self.memory_manager.cur.execute.assert_called()
+        self.memory_manager.conn.commit.assert_called()
+        call_args = self.memory_manager.cur.execute.call_args
+        sql_query, params = call_args[0]
+        assert type(params[0]) == str
+        assert params[1] == "test_role"
+        assert params[2] == "test_content"
+        assert (
+            params[3] > 0
+        ), f"Expected tokens to be greater than 0, but got {params[3]}"
+        assert params[4] is None
+        assert params[5] is None
+        assert params[6] is not None
