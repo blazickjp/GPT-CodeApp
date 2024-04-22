21962574678b9c9af421d16ac8d26dd7d3fb8c0a
blazickjp <joe.blazick@yahoo.com>
2024-04-21 18:49:54 -0700

    new gpt model

--- frontend/components/ModelSelector.js
+++ frontend/components/ModelSelector.js
@@ -83,7 +83,7 @@ const ModelSelector = () => {
     return (
         <div className='inline-flex justify-center bg-gray-700 p-2 rounded-lg'> {/* You can adjust the color (bg-gray-800), padding (p-2), and roundness (rounded-lg) as needed */}
             <Button id="gpt-3.5-turbo-1106" icon={<GiLightningBranches className=' text-green-500 mx-2' />} text="GPT-3.5" />
-            < Button id="gpt-4-0125-preview" icon={< GiStarsStack className='text-purple-500 mx-2' />} text="GPT-4" />
+            < Button id="gpt-4-turbo" icon={< GiStarsStack className='text-purple-500 mx-2' />} text="GPT-4" />
             < Button id="anthropic" icon={< FaAmazon className='text-yellow-500 mx-2' />} text="Anth" />
         </div >
     );
